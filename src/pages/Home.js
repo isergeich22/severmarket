2142394f0dd6bfe719edc8c924c1d4f3fa606d20
mobynick <mobynick365@gmail.com>
2023-09-19 23:02:43 +0300

    add search result

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "../components/Header";
 import SearchForm from "../components/subcomponents/Search";
 import Commend from "../components/Commend";
 import Cooperation from "../components/Cooperation";
- 
+import {answer} from "../stub-integration/data";
+import Products from "../components/subcomponents/products.json";
+import SearchResult from "../components/SearchResult";
+
+
+const initialState = {answer: '', goods: []};
+
 export default function Home() {
+    const [searchContext, setSearchContext] = useState(initialState);
 
     return(
 
         <React.Fragment>
             <Header />
-            <SearchForm />
+            <SearchForm doSearch={text => text
+                ? setSearchContext({answer: answer, goods: Products})
+                : setSearchContext(initialState)} />
+            <SearchResult answer={searchContext.answer} goods={searchContext.goods} />
             <Commend />
             <Cooperation />
         </React.Fragment>
